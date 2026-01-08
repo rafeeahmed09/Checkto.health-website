c7f659fba752485090b2e9229cd97b0591e21593
Rafeeahmed09 <abdulrafee0078@gmail.com>
2026-01-08 19:39:43 +0530

    new committed

---
+++ script.js
@@ -0,0 +1,18 @@
+ /*</div>
+                <div class="e-con-inner">
+                    <div class="e-con-Audio call">
+                        <h2 class="Audio">Audio Call</h2>
+                        <h2 class="Therapy">Therapy</h2>
+                        <h4 class="6min">₹6/min</h4>
+                    </div>
+
+                    <div class="elementor-spacer-inner"></div>
+                </div>
+
+
+                <div class="e-con-video call">
+
+                    <h2 class="Video">Video Call</h2>
+                </div>
+                <h2 class="Therapy">Therapy</h2>
+                <h4 class="7min">₹7/min</h4> */
